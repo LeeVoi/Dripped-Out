466942c8ac0c828bdd36f8dffc2b304811cd50fe
JulianHesberg <julianhesbergdk@gmail.com>
2023-12-14 14:03:26 +0100

    cart item component

--- api/Front-Drip/src/app/cart/cart.component.ts
+++ api/Front-Drip/src/app/cart/cart.component.ts
@@ -1,11 +1,12 @@
 import {Component, inject} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {TopBarComponent} from "../top-bar/top-bar.component";
+import {CartItemComponent} from "../cart-item/cart-item.component";
 
 @Component({
   selector: 'cart',
   standalone: true,
-  imports: [CommonModule, TopBarComponent],
+  imports: [CommonModule, TopBarComponent, CartItemComponent],
   templateUrl: 'cart.component.html',
   styleUrls: ['cart.component.css']
 })
