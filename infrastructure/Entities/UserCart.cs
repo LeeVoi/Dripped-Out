d0be7536cae2e05c59396f0340dc3b9c59713070
LeeVoi <Abual3a7ed.6@gmail.com>
2023-12-14 16:02:54 +0100

    Added some more methods into the IUserProdMapper.cs, tested but needs a fix with some values being displayed twice.

--- infrastructure/Entities/UserCart.cs
+++ infrastructure/Entities/UserCart.cs
@@ -5,5 +5,11 @@
         public int UserId { get; set; }
         
         public int ProductId { get; set; }
+        
+        public int ColorId { get; set; }
+        
+        public int SizeId { get; set; }
+        
+        public int Quantity { get; set; }
     }
 }
