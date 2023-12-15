04fc25c9e02b4678bc4bafb940763c2cf30e8f36
L4ZIE <davidwymon@yahoo.com>
2023-12-15 21:04:40 +0100

    added ui for both cart and likes

--- api/Front-Drip/src/app/cart/cart.component.ts
+++ api/Front-Drip/src/app/cart/cart.component.ts
@@ -2,11 +2,12 @@ import {Component, inject} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {TopBarComponent} from "../top-bar/top-bar.component";
 import {CartItemComponent} from "../cart-item/cart-item.component";
+import {ProductCardComponent} from "../product-card/product-card.component";
 
 @Component({
   selector: 'cart',
   standalone: true,
-  imports: [CommonModule, TopBarComponent, CartItemComponent],
+  imports: [CommonModule, TopBarComponent, CartItemComponent, ProductCardComponent],
   templateUrl: 'cart.component.html',
   styleUrls: ['cart.component.css']
 })
