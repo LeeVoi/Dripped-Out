9b3cc17fb45badbb45c4196fd27d84f15dc0a45e
L4ZIE <davidwymon@yahoo.com>
2023-12-20 14:59:45 +0100

    added a dynamic view for cartUI

--- api/Front-Drip/src/app/cart/cart.component.ts
+++ api/Front-Drip/src/app/cart/cart.component.ts
@@ -1,8 +1,12 @@
-import {Component, inject} from "@angular/core";
+import {Component, inject, OnInit} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {TopBarComponent} from "../top-bar/top-bar.component";
 import {CartItemComponent} from "../cart-item/cart-item.component";
 import {ProductCardComponent} from "../product-card/product-card.component";
+import {Product, UserCartItem, UserLikesItem} from "../models";
+import {UserService} from "../services/userservice";
+
+
 
 @Component({
   selector: 'cart',
@@ -12,6 +16,26 @@ import {ProductCardComponent} from "../product-card/product-card.component";
   styleUrls: ['cart.component.css']
 })
 
-export class CartComponent{
+export class CartComponent implements OnInit {
+  showBackground: boolean = true;
+  userCartItems: UserCartItem[] = [];
 
+  constructor(private userService: UserService) {
+  }
+
+  async ngOnInit() {
+    await this.userService.getCartItems();
+    this.userCartItems = this.userService.userCartItems;
+  }
+
+  async removeProduct(item: UserCartItem) {
+    await this.userService.removeFromCart(item);
+    await this.userService.getCartItems();
+    this.userCartItems = this.userService.userCartItems;
+  }
+
+  async addToFavorites(item: UserCartItem) {
+   await this.userService.addToLiked(item);
+
+  }
 }
